9c71485a30ac79bcf99b11db246b69bf423aa202
Telminov Sergey <telminov@soft-way.biz>
2015-05-10 11:47:40 +0300

    correction of package distribution

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name='sw-django-utils',
-    version='0.0.3',
+    version='0.0.6',
     description='Soft Way company django utils.',
     author='Telminov Sergey',
     url='https://github.com/telminov/sw-django-utils',
-    packages=['djutils',],
+    packages=['djutils', 'djutils.views', 'djutils.tests'],
     license='The MIT License',
     install_requires=[
         'django',
