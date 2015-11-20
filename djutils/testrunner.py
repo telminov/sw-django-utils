38019cde13dbc1a498e646bfe3e7f3517683949f
sergey@telminov.ru <sergey@telminov.ru>
2015-11-20 20:05:43 +0300

    refactoring

--- djutils/testrunner.py
+++ djutils/testrunner.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 from django.conf import settings
 from django.test.runner import DiscoverRunner
+
 
 class TestRunnerWithMongo(DiscoverRunner):
     mongodb_name = 'test_%s' % (settings.MONGODB['NAME'], )
@@ -24,14 +26,14 @@ class TestRunnerWithMongo(DiscoverRunner):
         from mongoengine.connection import connect, disconnect, register_connection
 
         # main mongo database
-        disconnect()
+        # disconnect()
         connect(self.mongodb_name, **self._get_params())
-        print 'Creating mongo test database %s for alias \'default\'' % self.mongodb_name
+        print('Creating mongo test database %s for alias \'default\'' % self.mongodb_name)
 
         # additional databases
         addition_aliases = self.get_addition_mongodb_aliases()
         for alias in addition_aliases:
-            print 'Creating mongo test database %s for alias \'%s\'' % (self.mongodb_name, alias)
+            print('Creating mongo test database %s for alias \'%s\'' % (self.mongodb_name, alias))
             disconnect(alias=alias)
             register_connection(alias, self.mongodb_name, **self._get_params())
 
@@ -44,10 +46,10 @@ class TestRunnerWithMongo(DiscoverRunner):
         connection = get_connection()
         try:
             connection.drop_database(self.mongodb_name)
-            print 'Dropping mongo test database %s for alias \'default\'' % self.mongodb_name
+            print('Dropping mongo test database %s for alias \'default\'' % self.mongodb_name)
         except Exception as ex:
-            print 'Can not to drop mongo test database %s for alias \'default\': %s' % (self.mongodb_name, ex)
-        disconnect()
+            print('Can not to drop mongo test database %s for alias \'default\': %s' % (self.mongodb_name, ex))
+        # disconnect()
 
         # additional databases
         addition_aliases = self.get_addition_mongodb_aliases()
@@ -55,9 +57,9 @@ class TestRunnerWithMongo(DiscoverRunner):
             connection = get_connection(alias=alias)
             try:
                 connection.drop_database(self.mongodb_name)
-                print 'Dropping mongo test database %s for alias \'%s\'' % (self.mongodb_name, alias)
+                print('Dropping mongo test database %s for alias \'%s\'' % (self.mongodb_name, alias))
             except Exception as ex:
-                print 'Can not to drop mongo test database %s for alias \'%s\': %s' % (self.mongodb_name, alias, ex)
+                print('Can not to drop mongo test database %s for alias \'%s\': %s' % (self.mongodb_name, alias, ex))
             disconnect()
 
         super(TestRunnerWithMongo, self).teardown_databases(old_config, **kwargs)
