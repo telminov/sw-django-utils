64beb240d755566d79e87ec452589858bb01482e
sergey@telminov.ru <sergey@telminov.ru>
2016-02-21 19:42:43 +0300

    add pretty_json template filter

--- djutils/templatetags/djutils.py
+++ djutils/templatetags/djutils.py
@@ -1,8 +1,11 @@
 # coding: utf-8
+import json
+
 from django import template
 import sqlparse
 
 register = template.Library()
+
 
 @register.filter('pretty_sql')
 def pretty_sql(sql):
@@ -11,3 +14,12 @@ def pretty_sql(sql):
         return sql
     except Exception:
         return sql
+
+
+@register.filter('pretty_json')
+def pretty_json(json_text):
+    try:
+        pretty_json_text = json.dumps(json_text, indent=4)
+        return pretty_json_text
+    except Exception:
+        return json_text
