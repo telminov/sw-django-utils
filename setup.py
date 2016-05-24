cf484ccad671207f24e1d8c7d96294c37ffc9486
sergey@telminov.ru <sergey@telminov.ru>
2016-05-24 14:10:18 +0300

    correct setup file for including template-directory

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 # coding: utf-8
 # python setup.py sdist register upload
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sw-django-utils',
-    version='0.0.23',
+    version='0.0.24',
     description='Soft Way company django utils.',
     author='Telminov Sergey',
+    author_email='sergey@telminov.ru',
     url='https://github.com/telminov/sw-django-utils',
-    packages=[
-        'djutils',
-        'djutils.views',
-        'djutils.tests',
-        'djutils.templatetags',
-        'djutils.management',
-        'djutils.management.commands',
-    ],
+    include_package_data=True,
+    packages=find_packages(),
     license='The MIT License',
-    install_requires=[
+    requires=[
         'django',
-        'sqlparse==0.1.16',
+        'sqlparse',
     ],
 )
